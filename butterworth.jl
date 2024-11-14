818cfdd3eb3dab15ba44924acc03badec4ef40d8
Magnus N. Malmquist <magnusnym97@gmail.com>
2024-11-14 21:48:23 +0100

    Refactor: Butterworth filter module
    Organize the code for the Butterworth filter into a module.

---
+++ butterworth.jl
@@ -0,0 +1,52 @@
+module Butterworth
+
+export transfer
+
+include("rational.jl")
+
+import Polynomials: Polynomial
+import .RationalFunctions: RationalFunction, RationalFunctions
+
+"""Butterworth polynomial coefficients of a given order (> 0).
+
+Using the product formula from
+https://en.m.wikipedia.org/wiki/Butterworth_filter
+
+"""
+function butterworth(order::Int)
+  gamma = pi / (2.0 * order)
+  res = fill(1.0, order + 1)
+
+  # Since a_k = a_(n - k) we only need to iterate to n/2
+  # In a 0-indexed language substitute k -> k - 1 inside [...]
+  for k = 1:round(Int, order / 2)
+    coeff = (cos((k - 1) * gamma) / sin(k * gamma)) * res[k]
+    res[k + 1] = coeff
+    res[order - k + 1] = coeff
+  end
+  res
+end
+
+function z_to_s_plane(z::ComplexF64, sample_rate::Float64)
+  # Bilinear transform from z to s plane
+  2.0 * sample_rate * (z - 1) / (z + 1)
+end
+
+"""Discrete-time transfer function for a Butterworth filter
+
+With a given order, cutoff in hz and sample rate returns the transfer function
+as a function of a single complex variable.
+
+Based on the analog Butterworth filter with transfer function defined in
+s-plane from https://en.m.wikipedia.org/wiki/Butterworth_filter. To turn into
+an equivalent digital filter we apply a bilinear transform. Do the same
+analytically to get the difference equation coefficients for an implementation.
+
+"""
+function transfer(order::Int, cutoff::Float64, sample_rate::Float64)
+  fraction = RationalFunction(Polynomial([1.0]), Polynomial(butterworth(order)))
+  cutoff_omega = cutoff * 2.0 * pi
+  z -> RationalFunctions.eval(z_to_s_plane(z, sample_rate) ./ cutoff_omega, fraction)
+end
+
+end
--- main.jl
+++ main.jl
@@ -1,34 +1,9 @@
+include("butterworth.jl")
+
 import Plots
 import Polynomials: Polynomial
 
-include("rational.jl")
-
-import .RationalFunctions: RationalFunction, RationalFunctions
-
-"""Butterworth polynomial coefficients of a given order (> 0).
-
-Using the product formula from
-https://en.m.wikipedia.org/wiki/Butterworth_filter
-
-"""
-function butterworth(order::Int)
-  gamma = pi / (2.0 * order)
-  res = fill(1.0, order + 1)
-
-  # Since a_k = a_(n - k) we only need to iterate to n/2
-  # In a 0-indexed language substitute k -> k - 1 inside [...]
-  for k = 1:round(Int, order / 2)
-    coeff = (cos((k - 1) * gamma) / sin(k * gamma)) * res[k]
-    res[k + 1] = coeff
-    res[order - k + 1] = coeff
-  end
-  res
-end
-
-# Bilinear transform from z to s plane
-function z_to_s_plane(z::ComplexF64, sample_rate::Float64)
-  return 2.0 * sample_rate * (z - 1) / (z + 1)
-end
+import .Butterworth
 
 function db(gain::Float64)
   10 * log(10, gain)
@@ -47,30 +22,8 @@ function main()
   start_freq = 26.0
   end_freq = sample_rate / 2 - 1050
 
-  # TODO: This is supposed to be third order Butterworth, so something is not correct.
-  # Maybe because this was taken from the analog version?
-
-  # Analog Butterworth filter with transfer function defined in s-plane
-  # From https://en.m.wikipedia.org/wiki/Butterworth_filter
-  # To turn into digital filter difference equation apply bilinear transform analytically.
-  #
-  # Third order
-  # de_butter_s = DifferenceEquation([1.0], [1.0, 2.0, 2.0, 1.0])
-  #
-  # Tenth order
-  # TODO: Generate the coefficients
-  # TODO: Find the difference equation for the discrete time variant after applying
-  #       bilinear transform.
-  de_butter_s_order_10 = RationalFunction(Polynomial([1.0]), Polynomial(butterworth(10)))
-
-  # The substitution s -> s / omega_c gives a Butterworth with a given cutoff freq
-  cutoff_freq = 740.0
-  cutoff_omega = cutoff_freq * 2.0 * pi
-
   # Transfer function
-  h = z -> RationalFunctions.eval(z_to_s_plane(z, sample_rate) ./ cutoff_omega, de_butter_s_order_10)
-
-  # h = z -> transfer_function(z_to_s_plane(z, sample_rate) ./ cutoff_omega, de_butter_s_order_10)
+  h = Butterworth.transfer(10, 500.0, sample_rate)
 
   # Frequencies are perceived logarithmically
   freqs = logrange(start_freq, end_freq, length=500)
