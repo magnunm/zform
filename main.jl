283bb40f4905fad724173b3804377738ef23dccf
Magnus N. Malmquist <magnusnym97@gmail.com>
2024-11-17 14:33:29 +0100

    Extract Bode plot generation to module
    Refactor

--- main.jl
+++ main.jl
@@ -1,65 +1,19 @@
 include("butterworth.jl")
+include("plotting.jl")
 
 import Plots
 import Polynomials: Polynomial
 
 import .Butterworth
-
-function db(gain::Float64)
-  10 * log(10, gain)
-end
-
-function rad_to_deg(angle::Float64)
-  (180 * angle) / pi
-end
+import .Plotting
 
 function main()
-  # Typical sample rate for audio
   sample_rate = 44100.0
-
-  # Around (sample rate / 2) the analysis breaks down and we get strange results
-  # so end before that.
-  start_freq = 26.0
-  end_freq = sample_rate / 2 - 1050
 
   # Transfer function
   h = Butterworth.transfer(10, 500.0, sample_rate)
 
-  # Frequencies are perceived logarithmically
-  freqs = logrange(start_freq, end_freq, length=500)
-  freqs_in_z_domain = freqs ./ sample_rate
-  zs = map(f -> cis(2.0 * pi * f), freqs_in_z_domain)
-  hs = h.(zs)
-
-  gains = abs.(hs)
-  phase_shifts = angle.(hs)
-
-  gain_plot = Plots.plot(
-    freqs,
-    db.(gains),
-    xscale=:log10,
-    minorgrid=true,
-    xticks=logrange(start_freq, end_freq, length=5),
-    xformatter=x -> string(round(Int, x)),
-    ylabel="Gain (dB)"
-  )
-  phase_plot = Plots.plot(
-    freqs,
-    rad_to_deg.(phase_shifts),
-    xscale=:log10,
-    minorgrid=true,
-    ylims=(-180, 180),
-    yticks=range(-180, 180, length=5),
-    xticks=logrange(start_freq, end_freq, length=5),
-    xformatter=x -> string(round(Int, x)),
-    ylabel="Phase Shift (degrees)",
-    xlabel="Frequency"
-  )
-
-  Plots.plot(gain_plot, phase_plot, layout=(2, 1), legend=false)
-
-  # Switch to SVG for better scalability of graphic
-  Plots.savefig("plot.png")
+  Plotting.bode(h, sample_rate)
 end
 
 main()
---
+++ plotting.jl
@@ -0,0 +1,65 @@
+module Plotting
+
+export bode
+
+import Plots
+
+function db(gain::Float64)
+  10 * log(10, gain)
+end
+
+function rad_to_deg(angle::Float64)
+  (180 * angle) / pi
+end
+
+"""Bode plot for a discrete time transfer function.
+
+Plot the gain and phase response as a function of frequency for a given
+transfer function in the z-domain. The transfer function is a complex function
+of a single complex variable.
+
+"""
+function bode(transfer::Function, sample_rate::Float64)
+  # Around (sample rate / 2) the analysis breaks down and we get strange results
+  # so end before that.
+  start_freq = 26.0
+  end_freq = sample_rate / 2 - 1050
+
+  # Frequencies are perceived logarithmically
+  freqs = logrange(start_freq, end_freq, length=500)
+  freqs_in_z_domain = freqs ./ sample_rate
+  zs = map(f -> cis(2.0 * pi * f), freqs_in_z_domain)
+  hs = transfer.(zs)
+
+  gains = abs.(hs)
+  phase_shifts = angle.(hs)
+
+  gain_plot = Plots.plot(
+    freqs,
+    db.(gains),
+    xscale=:log10,
+    minorgrid=true,
+    xticks=logrange(start_freq, end_freq, length=5),
+    xformatter=x -> string(round(Int, x)),
+    ylabel="Gain (dB)"
+  )
+  phase_plot = Plots.plot(
+    freqs,
+    rad_to_deg.(phase_shifts),
+    xscale=:log10,
+    minorgrid=true,
+    ylims=(-180, 180),
+    yticks=range(-180, 180, length=5),
+    xticks=logrange(start_freq, end_freq, length=5),
+    xformatter=x -> string(round(Int, x)),
+    ylabel="Phase Shift (degrees)",
+    xlabel="Frequency"
+  )
+
+  Plots.plot(gain_plot, phase_plot, layout=(2, 1), legend=false)
+
+  # Switch to SVG for better scalability of graphic
+  Plots.savefig("plot.png")
+end
+
+end
